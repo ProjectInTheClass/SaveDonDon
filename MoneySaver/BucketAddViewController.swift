f65cbccae6d42baf75c9f4a0e4f1d9aa9b824973
jmzzang <dream7739@naver.com>
2018-07-21 11:54:10 +0900

    [정민] 런치스크린 오토레이아웃 / 버킷리스트 오토레이아웃 적용중

---
+++ MoneySaver/BucketAddViewController.swift
@@ -0,0 +1,19 @@
+
+
+import UIKit
+
+class BucketAddViewController: UIViewController {
+
+    let modelBucket = BucketModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
--- MoneySaver/BucketViewController.swift
+++ MoneySaver/BucketViewController.swift
@@ -33,9 +33,9 @@ class BucketViewController: UIViewController, UITableViewDataSource, UITableView
         let info = modelBucket.arrayList[indexPath.row]
         let cell:BucketCell = tableView.dequeueReusableCell(withIdentifier: "BucketCell") as! BucketCell
         cell.goalName.text = info.goalName
-        cell.goalMoney.text = String(info.goalMoney)
+        cell.goalMoney.text = String(info.goalMoney)+"원"
         cell.goalImage.image = UIImage(named: info.goalImg)
-        cell.donMoney.text = String(info.donMoney)
+        cell.donMoney.text = String(info.donMoney)+"원"
         cell.donNum.text = String(info.donNum)
         
         return cell
@@ -46,18 +46,34 @@ class BucketViewController: UIViewController, UITableViewDataSource, UITableView
         if editingStyle == UITableViewCellEditingStyle.delete{
             self.modelBucket.arrayList.remove(at:indexPath.row) //데이터 삭제
             tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+
         }
     }
     
-    //다음 창으로 넘어갈 때
+    //다음 창으로 넘어갈 때 준비함
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //연결된 세그가 테이블에서 연결된 편집창
+        if segue.identifier == "editBucket"{
         let cell = sender as! BucketCell //내가 누른 셀
         let indexPath:IndexPath = table.indexPath(for: cell)! //내가 누른 셀의 indexPath
         self.modelBucket.selectedIndex = indexPath.row //누른 곳의 인덱스를 indexPath.row로
         let bucketSetting = segue.destination as! BucketSettingController //목적지는 버킷리스트 세팅창
         bucketSetting.modelBucket = self.modelBucket //bucketSetting으로 modelbucket넘겨줌
-    }
+        }
+        
+        //연결된 세그가 버킷리스트 추가 버튼
+        if segue.identifier == "addBucket"{
+        }
+        
+}
+    
+    
+    
+    
     
     
     
 }
+    
+
