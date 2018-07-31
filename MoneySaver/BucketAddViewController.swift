0acc7af8047984971306e5f876fc7f23e9f9101a
JYP in MBP 15 <me@ebadaq.com>
2018-07-31 17:47:17 +0900

    고민~해결!! 이라기보다도, 문제를 확인해 보길 바랍니다. ;)

--- MoneySaver/BucketAddViewController.swift
+++ MoneySaver/BucketAddViewController.swift
@@ -105,7 +105,12 @@ class BucketAddViewController: UIViewController, UIImagePickerControllerDelegate
             let bucketVC = segue.destination as? BucketViewController
             bucketVC?.addNewInfo() //테이블뷰 갱신(메인)
         }
-        
-        
+    }
+    // prepare 전에 이 메서드가 호출됨. true 이면 segue 가 동작하기 시작하고, false 이면 동작 안함.
+    // 지금 안되는 건, segue의 unwind 가 동작하는데, 화면 넘어가면 안되는데 계속 segue는 타는데,
+    // alert이 올라와 있기 때문에, 경고 나면서 멈추는 거에요.
+    // 위의 prepare 메서드와 아래 should 메서드에 break point 걸고 테스트 해보세요. :) 이해 될꺼임.
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return false
     }
 }
